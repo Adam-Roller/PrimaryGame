4ea1176a405ae9e2535eac41eea5760bdb55b568
Adam Roller <adam.c.roller@gmail.com>
2024-12-10 21:17:52 -0500

    Add Double Jump

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 100.0
-@export var JUMP_VELOCITY = -200.0
+@export var speed = 100.0
+@export var jump_velocity = -300.0
 
 @export_flags("DOUBLE_JUMP", "DASH", "WALL_CLING") var abilities = 0
+
+var jumps_left = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,18 +18,23 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	# Reset jumps on floor
+	if is_on_floor():
+		jumps_left = 1 + int((abilities & Global.ABILITY_TYPES.Double_Jump != 0))
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and jumps_left > 0:
+		velocity.y = jump_velocity
+		jumps_left -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 	
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,9 +1,9 @@
 extends Node
 
-enum ABILITY_TYPES {Double_Jump, Dash, Wall_Cling}
+enum ABILITY_TYPES {Double_Jump = 1, Dash = 2, Wall_Cling = 4}
 
 func color_from_flag(flag: int) -> Color:
 	return Color(
-		flag & (1 << Global.ABILITY_TYPES.Double_Jump), 
-		flag & (1 << Global.ABILITY_TYPES.Dash), 
-		flag & (1 << Global.ABILITY_TYPES.Wall_Cling))
+		flag & ABILITY_TYPES.Double_Jump, 
+		flag & ABILITY_TYPES.Dash, 
+		flag & ABILITY_TYPES.Wall_Cling)
